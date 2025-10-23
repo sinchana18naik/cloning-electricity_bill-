4ac1af39517352f214ff4158f2c277dfa4f2e5d3
sinchana18naik <sinchananaik112005@gmail.com>
2025-10-23 15:59:53 +0530

    save to new branch

--- electricity_bill.py
+++ electricity_bill.py
@@ -1,5 +1,12 @@
 units = int(input("Enter units consumed: "))
 rate_per_unit = 5
 total_bill = units * rate_per_unit
+if total_bill >1000:
+    discount = total_bill*0.10
+else:
+    discount = 0
+final_bill = total_bill - discount
 print("\nUnits Consumed:", units)
 print("Total Bill: ₹", total_bill)
+print("dicount applied :",discount)
+print("final bill:",final_bill)
